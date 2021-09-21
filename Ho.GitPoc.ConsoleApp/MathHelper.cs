ed0d7b3908da5fa4a92c7adb82e1b38a4d9e4833
Hani Ho <hani.herbert.ho@gmail.com>
2021-09-21 22:24:25 +0800

    feat: Scaffolded IsOddOrEven().

---
+++ Ho.GitPoc.ConsoleApp/MathHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ho.GitPoc.ConsoleApp
+{
+    public static class MathHelper
+    {
+        public static bool IsOddOrEven(int myInteger)
+        {
+            return true;
+        }
+    }
+}
