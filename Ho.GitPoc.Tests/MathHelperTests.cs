238ff552cff7d3e67f4affc8271aa9c79850f7c1
Hani Ho <hani.herbert.ho@gmail.com>
2021-09-21 22:33:40 +0800

    test: Added tests for high-value numbers.

--- Ho.GitPoc.Tests/MathHelperTests.cs
+++ Ho.GitPoc.Tests/MathHelperTests.cs
@@ -7,15 +7,27 @@ namespace Ho.GitPoc.Tests
     public class MathHelperTests
     {
         [Fact]
-        public void TestForEvenLowEvenNumbers()
+        public void TestForLowEvenNumbers()
         {
             Assert.True(MathHelper.IsOddOrEven(2));
         }
 
         [Fact]
-        public void TestForOddLowEvenNumbers()
+        public void TestForLowOddNumbers()
         {
             Assert.False(MathHelper.IsOddOrEven(1));
+        }
+
+        [Fact]
+        public void TestForHighEvenNumbers()
+        {
+            Assert.True(MathHelper.IsEven(2048));
+        }
+
+        [Fact]
+        public void TestForHighOddNumbers()
+        {
+            Assert.True(MathHelper.IsEven(2049));
         }
     }
 }
