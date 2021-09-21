2eaeea1ed3a8044d87e7c4b36cd665376a0b7481
Hani Ho <hani.herbert.ho@gmail.com>
2021-09-21 22:12:54 +0800

    feat: Initialized project.

---
+++ Ho.GitPoc.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ho.GitPoc.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
